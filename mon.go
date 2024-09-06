aeb2e5e27e826735d58b8e296da597f991d52100
ashestosea <ashestosea@proton.me>
2024-09-06 16:07:33 -0500

    Implement mon.ChangeForm

--- mon.go
+++ mon.go
@@ -3,6 +3,9 @@ package main
 //go:generate go-enum --marshal --names --values --nocase
 
 import (
+	"math/rand"
+
+	"fmt"
 	"github.com/google/uuid"
 )
 
@@ -66,15 +69,49 @@ func (mon *Mon) AgeUp(sanctuary Sanctuary, region Region) {
 }
 
 func (mon *Mon) ChangeForm(sanctuary Sanctuary, region Region) {
-	switch mon.MonType {
-	case FormEgg:
-		mon.MonType = FormBaby
-	case FormBaby:
-
-	case FormHead:
-	case FormSerpentine:
-	case FormInsectoid:
+	dietCounts := make(map[Food]int)
+	for _, food := range mon.Diet {
+		dietCounts[food] += 1
 	}
+
+	fmt.Println("CHANGE FORM____________________")
+
+	if mon.MonType == FormEgg {
+		mon.MonType = FormBaby
+	} else {
+		newFormWeights := make(map[Form]float32)
+		var total float32
+
+		for _, newForm := range region.GrowthChart[mon.MonType] {
+			for _, foodPref := range region.FoodPrefs[newForm] {
+				weight := float32(dietCounts[foodPref])
+				newFormWeights[newForm] = weight
+			}
+		}
+
+		for _, weight := range newFormWeights {
+			total += weight
+		}
+
+		fmt.Println("NewFormWeights")
+		for form, weight := range newFormWeights {
+			fmt.Println("  form = ", form, " weight = ", weight)
+		}
+		fmt.Println("   total = ", total)
+
+		fmt.Println("Selecting new form_____")
+		var remainingDist float32 = rand.Float32() * total
+		for newForm, weight := range newFormWeights {
+			remainingDist -= weight
+			fmt.Println("  remainingDist = ", remainingDist, " form = ", newForm)
+			if remainingDist < 0 {
+				mon.MonType = newForm
+				break
+			}
+		}
+	}
+
+	fmt.Println("newForm = ", mon.MonType)
 }
 
 const (
